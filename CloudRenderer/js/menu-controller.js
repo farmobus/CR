8a000c050c39f6ff6c325fa4af1d8c401e732a8d
farmobus <69415206+farmobus@users.noreply.github.com>
2022-03-02 20:19:30 +0300

    Delete CloudRenderer directory

--- CloudRenderer/js/menu-controller.js
+++
@@ -1,5 +0,0 @@
-function MenuController(){
-    menu = document.getElementById("menu");
-    if (menu.className == "menu-hidden"){menu.className = "menu-visible";}
-    else{menu.className = "menu-hidden";}
-}
